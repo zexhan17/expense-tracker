9dd45262f77d11a1bc160794498d377479a9f1cb
Zeeshan <77879378+zexhan17@users.noreply.github.com>
2023-11-09 19:57:23 +0500

    add info

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,13 +1,18 @@
 <script>
+    import Amount from "./Amount.svelte";
+    import Info from "./Info.svelte";
+
     export let income = "";
     export let expense = "";
     export let total = 0;
 </script>
 
-<h2 class="font-bold mb-5 text-xl uppercase text-center">Expense Tracker</h2>
+<h2 class="font-bold mb-5 text-xl uppercase text-center">
+    Expense Tracker <Info />
+</h2>
 <span class="flex justify-between items-center">
     <h3 class="font-medium">Total BALANCE</h3>
-    <h1 class="font-semibold text-2xl">{total}</h1>
+    <h1 class="font-semibold text-2xl"><Amount {total} /></h1>
 </span>
 
 <div class="mt-1 shadow bg-white rounded p-4 flex justify-evenly items-center">
